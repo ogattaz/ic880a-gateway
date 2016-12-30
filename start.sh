2c608433152b3edb6cf10aad1eefd958e0bd77ae
ogattaz <olivier@gattaz.com>
2016-12-30 15:35:35 +0100

    adjust trace lines

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-echo "[Loranet Gateway]: Reset iC880a PIN (25)..."
+echo "[loranet-gateway]: Reset iC880a PIN (25)..."
 SX1301_RESET_BCM_PIN=25
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export 
 echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction 
@@ -13,11 +13,11 @@ echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
 
-echo "[Loranet Gateway]: iC880a Reseted."
+echo "[loranet-gateway]: iC880a Reseted."
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[Loranet Gateway]: Waiting for internet connection..."
+  echo "[loranet-gateway]: Waiting for internet connection..."
   sleep 30
   done
 
@@ -25,5 +25,5 @@ while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
 
 
 # Fire up the forwarder.
-echo "[Loranet Gateway]: Launching lora_pkt_fwd..."
+echo "[loranet-gateway]: Launching lora_pkt_fwd..."
 ./lora_pkt_fwd
