e1c7b25adaa778d62c47e09bea817e9116faea32
ogattaz <olivier@gattaz.com>
2016-12-30 15:30:03 +0100

    Use lora_pkt_fwd rather than poly_pkt_fwd remove remote config support

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# Reset iC880a PIN
+
+echo "[Loranet Gateway]: Reset iC880a PIN (25)..."
 SX1301_RESET_BCM_PIN=25
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export 
 echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction 
@@ -12,44 +13,17 @@ echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
 sleep 0.1
 echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/unexport 
 
+echo "[Loranet Gateway]: iC880a Reseted."
+
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[TTN Gateway]: Waiting for internet connection..."
+  echo "[Loranet Gateway]: Waiting for internet connection..."
   sleep 30
   done
 
-# If there's a remote config, try to update it
-if [ -d ../gateway-remote-config ]; then
-    # First pull from the repo
-    pushd ../gateway-remote-config/
-    git pull
-    git reset --hard
-    popd
+# remote config isn't supported !
 
-    # And then try to refresh the gateway EUI and re-link local_conf.json
-
-    # Same eth0/wlan0 fallback as on install.sh
-    GATEWAY_EUI_NIC="eth0"
-    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
-        GATEWAY_EUI_NIC="wlan0"
-    fi
-
-    if [[ `grep "$GATEWAY_EUI_NIC" /proc/net/dev` == "" ]]; then
-        echo "ERROR: No network interface found. Cannot set gateway ID."
-        exit 1
-    fi
-
-    GATEWAY_EUI=$(ip link show $GATEWAY_EUI_NIC | awk '/ether/ {print $2}' | awk -F\: '{print $1$2$3"FFFE"$4$5$6}')
-    GATEWAY_EUI=${GATEWAY_EUI^^} # toupper
-
-    echo "[TTN Gateway]: Use Gateway EUI $GATEWAY_EUI based on $GATEWAY_EUI_NIC"
-    INSTALL_DIR="/opt/loranet-gateway"
-    LOCAL_CONFIG_FILE=$INSTALL_DIR/bin/local_conf.json
-
-    if [ -e $LOCAL_CONFIG_FILE ]; then rm $LOCAL_CONFIG_FILE; fi;
-    ln -s $INSTALL_DIR/gateway-remote-config/$GATEWAY_EUI.json $LOCAL_CONFIG_FILE
-
-fi
 
 # Fire up the forwarder.
-./poly_pkt_fwd
+echo "[Loranet Gateway]: Launching lora_pkt_fwd..."
+./lora_pkt_fwd
